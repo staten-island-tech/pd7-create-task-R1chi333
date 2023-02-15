a5b918d959b35208ac9e980bf88f339ec7a463bd
richie <113203265+R1chi333@users.noreply.github.com>
2023-02-15 13:47:22 -0500

    .

--- create-task/main.js
+++ create-task/main.js
@@ -1,18 +1,24 @@
+let maxcooktime = null
 const DOMSelectors = {
   container: document.getElementById("con"),
   userinputform: document.getElementById("userinputs"),
-  recipes: document.getElementById("recipes")
+  recipes: document.getElementById("recipes"),
+  maxcooktime: document.getElementById("maxcooktime")
 }
-
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'ad555dce74mshb1522482a48114dp1f8cddjsnff68c51d250d',
-		'X-RapidAPI-Host': 'kfc-chickens.p.rapidapi.com'
+		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
 	}
 };
-
-fetch('https://kfc-chickens.p.rapidapi.com/chickens/2', options)
+DOMSelectors.userinputform.addEventListener(`submit`,function(e){
+	e.preventDefault();
+	maxcooktime = DOMSelectors.maxcooktime.value
+	let url = `https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=listAPI`
+	console.log(maxcooktime)
+	fetch(url, options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+})
